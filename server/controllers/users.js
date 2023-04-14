b6bf717a372387986a87d5d70ca0dc994f59ea53
oskariruo <78789083+oskariruo@users.noreply.github.com>
2023-04-14 09:05:20 +0300

    addRemoveFriends function added

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,6 @@
 import User from "../models/User.js";
 
-/* READ */
+/* GETTING USER*/
 export const getUser = async (req, res) => {
     try {
         const { id } = req.params;
@@ -11,6 +11,7 @@ export const getUser = async (req, res) => {
     }
 }
 
+/* GETTING USER FRIENDS */
 export const getUserFriends = async (req, res) => {
     try {
         const { id } = req.params;
@@ -29,4 +30,37 @@ export const getUserFriends = async (req, res) => {
     } catch (err) {
         res.status(404).json({ message: err.message });
       }
+}
+
+/* UPDATING FRIENDS */
+export default addRemoveFriend = async (req, res) => {
+    try {
+        const { id, friendId } = req.params;
+        const user = await User.getById(id);
+        const friend = await User.getById(friendId);
+
+        if (user.friends.includes(friendId)) {
+            user.friends = user.friends.filter((id) => id !== friendId);
+            friend.friends = friend.friends.filter((id) => id !== id);
+        } else {
+            user.friends.push(friendId);
+            friend.friends.push(id);
+        }
+        await user.save();
+        await friend.save();
+        
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+      );
+
+      const formattedFriends = friends.map(
+        ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+          return { _id, firstName, lastName, occupation, location, picturePath };
+        }
+      );
+  
+      res.status(200).json(formattedFriends);
+    } catch (err) {
+      res.status(404).json({ message: err.message });
+    }
 }
